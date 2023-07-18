dd9ecb854238abc74d9a2677ef68892bd0b272a3
michaljarnot <m.jarnot@yahoo.com>
2023-07-18 16:17:48 +0200

    build: dockerize the application

--- apps/auth/src/modules/auth/auth.service.ts
+++ apps/auth/src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcryptjs'
 import { DeepPartial } from 'typeorm'
 import { RegisterRequestDTO } from '@app/auth/modules/auth/dto/register.dto'
 import { RefreshTokenRepository } from '@app/auth/modules/auth/entities/refresh-token-repository'
