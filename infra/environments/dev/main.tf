6d14b4987fa3b878933d873b1c82a51255770e76
michaljarnot <m.jarnot@yahoo.com>
2024-07-03 01:20:44 +0200

    feat: cloud run

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -47,3 +47,98 @@ module "rabbitmq" {
   environment = var.environment
   vpc_network = module.vpc.vpc_name
 }
+
+module "cloud_run_auth" {
+  source           = "../../modules/cloud_run"
+  project_id       = var.project_id
+  region           = var.region
+  environment      = var.environment
+  service_name     = "auth-service"
+  container_image  = "${var.region}-docker.pkg.dev/${var.project_id}/${module.artifact_registry.repository_id}/${var.environment}-auth-service:latest"
+  vpc_connector    = module.vpc.vpc_connector_name
+  cloudsql_instance = module.cloud_sql.connection_name
+
+  env_vars = {
+    NODE_ENV         = var.environment
+    APP_NAME         = "Auth Microservice"
+    HTTP_PORT        = "8081"
+    AMQP_PORT        = "5672"
+    AMQP_QUEUE_NAME  = "auth_queue"
+    DATABASE_HOST    = "/cloudsql/${module.cloud_sql.connection_name}"
+    DATABASE_NAME    = module.cloud_sql.database_names["auth"]
+    RABBITMQ_HOST    = module.rabbitmq.rabbitmq_internal_ip
+  }
+
+  secrets = [
+    {
+      name = "${var.environment}-db-user"
+      key  = "latest"
+    },
+    {
+      name = "${var.environment}-db-password"
+      key  = "latest"
+    },
+    {
+      name = "${var.environment}-gcp-auth-sa-key"
+      key  = "latest"
+    }
+  ]
+}
+
+module "cloud_run_events" {
+  source           = "../../modules/cloud_run"
+  project_id       = var.project_id
+  region           = var.region
+  environment      = var.environment
+  service_name     = "events-service"
+  container_image  = "${var.region}-docker.pkg.dev/${var.project_id}/${module.artifact_registry.repository_id}/${var.environment}-events-service:latest"
+  vpc_connector    = module.vpc.vpc_connector_name
+  cloudsql_instance = module.cloud_sql.connection_name
+
+  env_vars = {
+    NODE_ENV         = var.environment
+    APP_NAME         = "Events Microservice"
+    HTTP_PORT        = "8082"
+    AMQP_PORT        = "5672"
+    AMQP_QUEUE_NAME  = "events_queue"
+    DATABASE_HOST    = "/cloudsql/${module.cloud_sql.connection_name}"
+    DATABASE_NAME    = module.cloud_sql.database_names["events"]
+    RABBITMQ_HOST    = module.rabbitmq.rabbitmq_internal_ip
+    AUTH_AMQP_QUEUE  = "auth_queue"
+  }
+
+  secrets = [
+    {
+      name = "${var.environment}-db-user"
+      key  = "latest"
+    },
+    {
+      name = "${var.environment}-db-password"
+      key  = "latest"
+    },
+    {
+      name = "${var.environment}-gcp-auth-sa-key"
+      key  = "latest"
+    }
+  ]
+}
+
+module "cloud_run_gateway" {
+  source           = "../../modules/cloud_run"
+  project_id       = var.project_id
+  region           = var.region
+  environment      = var.environment
+  service_name     = "gateway-service"
+  container_image  = "${var.region}-docker.pkg.dev/${var.project_id}/${module.artifact_registry.repository_id}/${var.environment}-gateway-service:latest"
+  vpc_connector    = module.vpc.vpc_connector_name
+
+  env_vars = {
+    NODE_ENV         = var.environment
+    APP_NAME         = "Gateway Microservice"
+    HTTP_PORT        = "8080"
+    AUTH_HTTP_PORT   = "8081"
+    AUTH_HOST        = module.cloud_run_auth.service_url
+    EVENTS_HTTP_PORT = "8082"
+    EVENTS_HOST      = module.cloud_run_events.service_url
+  }
+}
--- infra/modules/cloud_run/main.tf
+++ infra/modules/cloud_run/main.tf
@@ -1,128 +1,99 @@
-//
 // Variables
 
-// Global
 variable "project_id" {
   description = "The GCP project ID"
+  type        = string
 }
 
 variable "region" {
   description = "The GCP region for resources"
+  type        = string
 }
 
 variable "environment" {
   description = "Deployment environment"
+  type        = string
 }
 
-// Services
-variable "repository_id" {
-  description = "Repository ID for the Docker image in Artifact Registry"
+variable "service_name" {
+  description = "The name of the Cloud Run service"
+  type        = string
 }
 
-variable "sql_connection_name" {
-  description = "Connection name for the Cloud SQL instance"
+variable "container_image" {
+  description = "The container image to deploy"
+  type        = string
 }
 
-variable "app_service_docker_image_name" {
-  description = "The name of the Docker image for the app service"
+variable "vpc_connector" {
+  description = "The VPC connector for the Cloud Run service"
+  type        = string
 }
 
-// App
-variable "app_api_prefix" {
-  description = "The API prefix"
+variable "cloudsql_instance" {
+  description = "The Cloud SQL instance connection name"
+  type        = string
+  default     = ""
 }
 
-variable "app_api_name" {
-  description = "The app name"
+variable "env_vars" {
+  description = "Environment variables for the service"
+  type        = map(string)
+  default     = {}
 }
 
-variable "app_api_auth_jwt_token_expires_in" {
-  description = "The JWT token expiration"
+variable "secrets" {
+  description = "Secrets to mount in the service"
+  type = list(object({
+    name = string
+    key  = string
+  }))
+  default = []
 }
 
-// Database
-variable "database_name" {
-  description = "The database name"
-}
-
-variable "db_user_secret"  {
-  description = "The secret name for the database user"
-}
-
-variable "db_password_secret" {
-  description = "The secret name for the database password"
-}
-
-//
 // Main
 
-resource "google_cloud_run_service" "app_service" {
-  name     = "${var.project_id}-${var.environment}-service"
+resource "google_cloud_run_service" "service" {
+  name     = "${var.project_id}-${var.environment}-${var.service_name}"
   location = var.region
 
   template {
     spec {
       containers {
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repository_id}/${var.environment}-${var.app_service_docker_image_name}:latest"
+        image = var.container_image
 
-        env {
-          name  = "API_PREFIX"
-          value = var.app_api_prefix
-        }
-        env {
-          name  = "APP_NAME"
-          value = var.app_api_name
-        }
-        env {
-          name  = "AUTH_JWT_TOKEN_EXPIRES_IN"
-          value = var.app_api_auth_jwt_token_expires_in
-        }
-        env {
-          name = "AUTH_JWT_SECRET"
-          value_from {
-            secret_key_ref {
-              name = "${var.environment}-auth-jwt-secret"
-              key  = "latest"
-            }
+        dynamic "env" {
+          for_each = var.env_vars
+          content {
+            name  = env.key
+            value = env.value
           }
         }
-        env {
-          name  = "DATABASE_HOST"
-          value = "/cloudsql/${var.sql_connection_name}"
-        }
-        env {
-          name = "DATABASE_USER"
-          value_from {
-            secret_key_ref {
-              name = var.db_user_secret
-              key  = "latest"
+
+        dynamic "env" {
+          for_each = var.secrets
+          content {
+            name = env.value.name
+            value_from {
+              secret_key_ref {
+                name = env.value.name
+                key  = env.value.key
+              }
             }
           }
-        }
-        env {
-          name = "DATABASE_PASSWORD"
-          value_from {
-            secret_key_ref {
-              name = var.db_password_secret
-              key  = "latest"
-            }
-          }
-        }
-        env {
-          name = "DATABASE_NAME"
-          value = var.database_name
-        }
-        env {
-          name = "NODE_ENV"
-          value = var.environment
         }
       }
     }
 
     metadata {
-      annotations = {
-        "run.googleapis.com/cloudsql-instances" = var.sql_connection_name
-      }
+      annotations = merge(
+        {
+          "run.googleapis.com/vpc-access-connector" = var.vpc_connector
+        },
+          var.cloudsql_instance != "" ? {
+          "run.googleapis.com/cloudsql-instances" = var.cloudsql_instance
+        } : {}
+      )
     }
   }
 
@@ -133,53 +104,35 @@ resource "google_cloud_run_service" "app_service" {
 }
 
 resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
-  name                  = "${var.project_id}-${var.environment}-neg"
+  name                  = "${var.project_id}-${var.environment}-${var.service_name}-neg"
   network_endpoint_type = "SERVERLESS"
   region                = var.region
 
   cloud_run {
-    service = google_cloud_run_service.app_service.name
+    service = google_cloud_run_service.service.name
   }
 }
 
 // IAM
 
-data "google_compute_default_service_account" "default" {
-  project = var.project_id
+resource "google_cloud_run_service_iam_member" "run_invoker" {
+  location = google_cloud_run_service.service.location
+  project  = var.project_id
+  service  = google_cloud_run_service.service.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
-
-resource "google_project_iam_member" "secret_manager_access" {
-  project = var.project_id
-
-  role    = "roles/secretmanager.secretAccessor"
-  member = "serviceAccount:${data.google_compute_default_service_account.default.email}"
-}
-
-# Note: If you have working load balancer then you can remove this. I am keeping it here for debugging purposes.
-resource "google_cloud_run_service_iam_policy" "app_service_iam" {
-  location   = google_cloud_run_service.app_service.location
-  project    = var.project_id
-  service    = google_cloud_run_service.app_service.name
-
-  policy_data = jsonencode({
-    "bindings": [
-      {
-        "role": "roles/run.invoker",
-        "members": [
-          "allUsers"
-        ]
-      }
-    ]
-  })
-}
-
 
 // Output
 
-output "cloudrun_neg_id" {
-  value = google_compute_region_network_endpoint_group.cloudrun_neg.id
+output "service_url" {
+  value = google_cloud_run_service.service.status[0].url
 }
 
-output "cloud_run_service_url" {
-  value = google_cloud_run_service.app_service.status[0].url
+output "service_name" {
+  value = google_cloud_run_service.service.name
+}
+
+output "neg_id" {
+  value = google_compute_region_network_endpoint_group.cloudrun_neg.id
 }
